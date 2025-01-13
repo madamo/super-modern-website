4117ee8b63c4626f3ff7875b30099113cc3e4d70
madamo <mattadamo@gmail.com>
2025-01-13 09:39:03 -0800

    Header cleanup

--- scripts.js
+++ scripts.js
@@ -1,6 +1,9 @@
 const searchIcon = document.getElementById("search-icon");
 const searchContainer = document.getElementById("search-container");
 const closeSearch = document.getElementById("close-search");
+const showMenu = document.getElementById("menu-icon");
+const menu = document.getElementById("menu");
+const closeMenu = document.getElementById("close-menu");
 
 searchIcon.addEventListener("click", () => {
     searchContainer.style.display = "flex";
@@ -8,4 +11,13 @@ searchIcon.addEventListener("click", () => {
 
 closeSearch.addEventListener("click", () => {
     searchContainer.style.display = "none";
+})
+
+showMenu.addEventListener("click", () => {
+    menu.style.left = "0"
+})
+
+closeMenu.addEventListener("click", () => {
+    menu.style.left = "100%";
+    // TO-DO: Make sure this is hidden from screen readers
 })
