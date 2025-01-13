036fba1d1ba7fc73c173ecff5ff5e01783994e87
madamo <mattadamo@gmail.com>
2025-01-13 14:34:15 -0800

    Logo wall and start of feature section

--- scripts.js
+++ scripts.js
@@ -4,6 +4,9 @@ const closeSearch = document.getElementById("close-search");
 const showMenu = document.getElementById("menu-icon");
 const menu = document.getElementById("menu");
 const closeMenu = document.getElementById("close-menu");
+const featureContainer = document.getElementById("features");
+const featureImgBg = document.getElementById("feature-img-column"); // will eventually need to change this
+
 
 searchIcon.addEventListener("click", () => {
     searchContainer.style.display = "flex";
@@ -21,3 +24,19 @@ closeMenu.addEventListener("click", () => {
     menu.style.left = "100%";
     // TO-DO: Make sure this is hidden from screen readers
 })
+
+document.addEventListener("scroll", (event) => {
+    console.log(window.scrollY);
+    if (window.scrollY > 1160) {
+        featureImgBg.style.backgroundColor = "green"
+    } else {
+        featureImgBg.style.backgroundColor = "lightpink"
+
+    }
+
+})
+
+/*featureContainer.addEventListener("scroll", (event) => {
+    //alert("scrolling!");
+    console.log(featureContainer.scrollTop);
+})*/
